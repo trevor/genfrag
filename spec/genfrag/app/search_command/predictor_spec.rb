2167f86de18beeec8255fa9a0fefe8fa73329c73
Pjotr Prins <pjotr.public01@thebird.nl>
2009-03-05 17:33:48 +0100

    Predictor spec

---
+++ spec/genfrag/app/search_command/predictor_spec.rb
@@ -0,0 +1,24 @@
+
+require File.expand_path(
+    File.join(File.dirname(__FILE__), %w[.. .. .. spec_helper]))
+    
+# --------------------------------------------------------------------------
+describe "Genfrag::App::PredictorCommand" do
+
+  it "should receive a resultset from the search command"
+
+  it "should receive what predictor method to use"
+
+  describe "test adjusted sizes with a resultset" do
+
+    it "should calculate a p-value for every match"
+
+    it "should return p-values and flags for outlier values"
+
+    it "should optionally return the classifiers"
+
+  end
+  
+end
+
+# EOF
